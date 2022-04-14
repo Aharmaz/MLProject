6e0dbc4d364fb47f48d6b686098df38ea27ade09
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2022-04-14 16:25:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def index():
     iq = request.form.get('iq')
     profile_score = request.form.get('profile_score')
     input_query = np.array([[cgpa,iq,profile_score]])
-    return input_query
+    result = model.predict(input_query)[0]
+    return str(result)
 #     return "Hello world"
 # @app.route('/predict',methods=['POST'])
 @app.route('/')
