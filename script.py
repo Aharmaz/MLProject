e1a636e339978a3bfcaad8d726080f533783d6fd
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2022-05-30 12:45:12 +0000

    Add files via upload

--- script.py
+++ script.py
@@ -105,3 +105,24 @@ d={
 }
 for i in d:
     s=s.replace(d[i],i)
+
+from flask import Flask,request,jsonify
+model = pickle.load(open('model.pkl','rb'))
+app = Flask(__name__)
+@app.route('/predict',methods=['POST'])
+# @app.route('/')
+def index():
+    return s
+#     return "Hello world"
+# @app.route('/predict',methods=['POST'])
+@app.route('/')
+def predict():
+    # cgpa = request.form.get('cgpa')
+    # iq = request.form.get('iq')
+    # profile_score = request.form.get('profile_score')
+    # input_query = np.array([[cgpa,iq,profile_score]])
+    # result = model.predict(input_query)[0]
+    # return jsonify({'placement':str(result)})
+    return "hello world"
+if __name__ == '__main__':
+    app.run(debug=True)
