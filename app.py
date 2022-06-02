c157104ddd5c7c0198d130639f22711fb3295a24
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2022-06-02 16:00:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -29,13 +29,14 @@ def index():
 # @app.route('/predict',methods=['POST'])
 @app.route('/')
 def predict():
+    data=pd.read_json("arabicPresidentJson.json")
     # cgpa = request.form.get('cgpa')
     # iq = request.form.get('iq')
     # profile_score = request.form.get('profile_score')
     # input_query = np.array([[cgpa,iq,profile_score]])
     # result = model.predict(input_query)[0]
     # return jsonify({'placement':str(result)})
-    return "s"
+    return data.shape
 
 
 
