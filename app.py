56957209f9b448333893c0aeb7a6c3a4273f7b4e
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2022-06-02 15:18:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,26 @@ app = Flask(__name__)
 
 
 
+
+@app.route('/predict',methods=['POST'])
+# @app.route('/')
+def index():
+    a="hello "
+    b="world!"
+    return "lah yn3l xitan"
+#     return "Hello world"
+# @app.route('/predict',methods=['POST'])
+@app.route('/')
+def predict():
+    # cgpa = request.form.get('cgpa')
+    # iq = request.form.get('iq')
+    # profile_score = request.form.get('profile_score')
+    # input_query = np.array([[cgpa,iq,profile_score]])
+    # result = model.predict(input_query)[0]
+    # return jsonify({'placement':str(result)})
+    return "s"
+if __name__ == '__main__':
+  
 import string
 import pandas as pd
 import pyarabic.araby as araby
@@ -149,23 +169,4 @@ for i in d:
 
 
 
-
-@app.route('/predict',methods=['POST'])
-# @app.route('/')
-def index():
-    a="hello "
-    b="world!"
-    return "lah yn3l xitan"
-#     return "Hello world"
-# @app.route('/predict',methods=['POST'])
-@app.route('/')
-def predict():
-    # cgpa = request.form.get('cgpa')
-    # iq = request.form.get('iq')
-    # profile_score = request.form.get('profile_score')
-    # input_query = np.array([[cgpa,iq,profile_score]])
-    # result = model.predict(input_query)[0]
-    # return jsonify({'placement':str(result)})
-    return "s"
-if __name__ == '__main__':
     app.run(debug=True)
